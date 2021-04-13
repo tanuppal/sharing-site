2e9772983f36dc0303fa769b58221d8a846b7871
Tanup pal <pal1993tanup@gmail.com>
2021-04-14 04:09:05 +0530

    Add routes and remove sqlite gem

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   
   devise_for :users, controllers: {sessions: 'users/sessions',  registrations: 'users/registrations'}
-
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get 'home', to: 'home#index'
    get '/my-shared-location', to: 'share_locations#my_shared_location', :as => :my_shared_location
