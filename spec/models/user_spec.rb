6573f3d4ca04ab2cc828ccfe92a99348086dd8f3
Tanup pal <pal1993tanup@gmail.com>
2021-04-14 01:34:03 +0530

    Add user model rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
+
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, :type => :model do
+  subject {
+    described_class.new(name: "Johndd", email: "tanupp@gmail.com", password: "tanupp1234")
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+   it "is not valid without a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
 end
